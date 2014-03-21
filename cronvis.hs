63e869d1cce04c455607486d8be6bf24571d41a0
Marcus Geiger <marcus.geiger@spqr-info.de>
2014-03-21 09:08:23 +0100

    Fixed csv output and simplification of cron commands.

--- cronvis.hs
+++ cronvis.hs
@@ -1,5 +1,4 @@
 -- cronvis.hs my 1st test in haskell to translate transform.py to haskell
-
 import Text.Regex
 import Data.Char (isSpace)
 import Data.List
@@ -10,18 +9,6 @@ import Data.Time.Clock
 import Data.Time.Clock.POSIX
 
 import qualified Data.Map as Map
-
--- -- Some test data
--- logData :: [String]
--- logData = [ "Mar 18 06:25:01 pwl1 /USR/SBIN/CRON[23921]: (root) CMD (/usr/bin/nice -n 19 /usr/projekt/tools/bin/cron.start2 bhs.read_hist 2>/dev/null)"
---           , "Mar 18 06:25:01 pwl1 /USR/SBIN/CRON[23935]: (root) CMD (/usr/projekt/bin/pwl_1.reklaupd 2>/dev/null)"
---           , "Mar 18 06:25:01 pwl1 /USR/SBIN/CRON[23941]: (root) CMD (/usr/projekt/bin/pwl_1.cron_jede_min 2>&1 | /usr/bin/logger -p local1.warn -t pwl_1.cron_jede_min)"
---           , "Mar 18 06:25:01 pwl1 /USR/SBIN/CRON[23951]: (root) CMD (/usr/bin/nice -n 19 /usr/projekt/bin/pinfo.start 30)"
---           , "Mar 18 06:25:01 pwl1 /foo/bar/baz: (root) trallala"
---           ]
-
--- logLine :: String
--- logLine = head logData
 
 trim :: String -> String
 trim = f . f
@@ -39,8 +26,11 @@ stringTrimmedUpToChar :: Char -> String -> String
 stringTrimmedUpToChar c s = trim s'
     where s' = takeWhile (/= c) s
 
+removeAbsolutePath :: String -> String
+removeAbsolutePath s = reverse $ takeWhile (/= '/') $ reverse s
+
 simplifyCmd :: String -> String
-simplifyCmd = stringTrimmedUpToChar '>' . stringTrimmedUpToChar ' ' .
+simplifyCmd = removeAbsolutePath. stringTrimmedUpToChar '>' . stringTrimmedUpToChar ' ' .
               removeNiceCmd . removeCronStartCmd . stringTrimmedUpToChar '#'
 
 extractCronJob :: String -> String
@@ -48,14 +38,14 @@ extractCronJob s =
     case match of
       Just value -> head value -- get first grouped match
       Nothing    -> "" -- maybe a cron error log line
-    where match = matchRegex cronCmd s
-              where cronCmd = mkRegex "^.*CMD \\((.*)\\)[[:blank:]]*$"
+    where match   = matchRegex cronCmd s
+          cronCmd = mkRegex "^.*CMD \\((.*)\\)[[:blank:]]*$"
 
 -- Convert a cron timestamp (which is missing the year) to a UTCTime with the current year
 parseCronTimestamp :: Integer -> String -> UTCTime
 parseCronTimestamp currentYear str = UTCTime correctedDay $ utctDayTime time
-    where time = readTime defaultTimeLocale "%b %d %H:%M:%S" str :: UTCTime
-          yearDelta = currentYear - 1970
+    where time         = readTime defaultTimeLocale "%b %d %H:%M" str :: UTCTime
+          yearDelta    = currentYear - 1970
           correctedDay = addGregorianYearsClip yearDelta $ utctDay time
 
 -- A type for a cronjob and its execution timestamp
@@ -64,16 +54,21 @@ type CronDateParser = (String -> UTCTime)
 
 parseCronLine :: CronDateParser -> String -> CronjobExecution
 parseCronLine dateParser s = (time, jobname)
-    where time = dateParser $ take 15 s
+    where time    = dateParser $ take 12 s -- take date without seconds
           jobname = simplifyCmd $ extractCronJob s
 
 -- Filter and parse all cron commands out of syslog log lines
 parseCronSyslog :: CronDateParser -> [String] -> [CronjobExecution]
-parseCronSyslog dateParser lines = map (parseCronLine dateParser) $ filter (isInfixOf "/USR/SBIN/CRON") lines
+-- parseCronSyslog dateParser lines = map (parseCronLine dateParser) cronLines
+parseCronSyslog dateParser lines = noEmpty $ parseIt
+    where cronLines = filter (isInfixOf "/USR/SBIN/CRON") lines
+          parseIt = map (parseCronLine dateParser) cronLines
+          noEmpty = filter (not . null . snd)
 
+-- Create list of UTCTimes in interval [start end[.
+-- take into account that the jobs are ordered by using takeWhile instead of filter
 between :: UTCTime -> UTCTime -> [CronjobExecution] -> [CronjobExecution]
- -- take into account that the jobs are ordered by using takeWhile instead of filter
-between start end = takeWhile (pred . fst) -- (\j -> pred $ fst j)
+between start end = takeWhile (pred . fst)
     where pred t = t >= start && t < end
 
 -- Map of jobnames and their execution times in UTCTime
@@ -91,8 +86,12 @@ timeRange start end stepSecs = iter s
               where current = posixSecondsToUTCTime (fromIntegral n :: NominalDiffTime)
 
 -- Simplified ISO8601 date format without seconds
+formatMinuteTimestampForOutput :: UTCTime -> String
+formatMinuteTimestampForOutput = formatTime defaultTimeLocale "%Y-%m-%dT%H:%M"
+
+-- Full ISO8601 date timestamp
 formatTimestampForOutput :: UTCTime -> String
-formatTimestampForOutput = formatTime defaultTimeLocale "%Y-%m-%dT%H:%M"
+formatTimestampForOutput = formatTime defaultTimeLocale "%Y-%m-%dT%H:%M:%S"
 
 -- Get just the current year
 getCurrentYear :: IO Integer
@@ -107,28 +106,29 @@ parseIso8601Timestamp s = readTime defaultTimeLocale "%Y-%m-%dT%H:%M" s :: UTCTi
 
 -- Full timestamp each N minutes
 csvColumnTimestamps :: UTCTime -> UTCTime -> Int -> String
-csvColumnTimestamps start end stepSecs = intercalate filler timestamps -- concat $ intersperse filler timestamps
+csvColumnTimestamps start end stepSecs = intercalate filler timestamps
     where fullTimestampSecs = 60
-          filler = concat $ replicate (fullTimestampSecs - 1) ";"
-          timestamps = map formatTimestampForOutput (timeRange start end (stepSecs * fullTimestampSecs))
+          filler     = concat $ replicate fullTimestampSecs ";"
+          range      = (timeRange start end (stepSecs * fullTimestampSecs ))
+          timestamps = map formatMinuteTimestampForOutput range
 
 csvColumnMinutes :: UTCTime -> UTCTime -> Int -> String
 csvColumnMinutes start end stepSecs = concatMap (\ut -> show(minute ut) ++ ";") range
     where minute ut = todMin (timeToTimeOfDay $ utctDayTime ut)
-          range = timeRange start end stepSecs
+          range     = timeRange start end stepSecs
 
 csvData :: UTCTime -> UTCTime -> Int -> Map.Map String [UTCTime] -> String
 csvData start end stepSecs jobnamesMap = concatMap printHelper jobnames
-    where jobnames = sort (Map.keys jobnamesMap)
-          pointPri times ut =
-              if ut `elem` times
-              then "R;"
-              else ";"
-          printHelper job =
+    where jobnames          = sort (Map.keys jobnamesMap)
+          pointPri times ut = if ut `elem` times
+                              then "R;"
+                              else ";"
+          printHelper job   =
               let times = Map.lookup job jobnamesMap
-              in case times of
-                   Just times' -> ";" ++ concatMap (pointPri times') (timeRange start end stepSecs) ++ "\n"
-                   Nothing     -> error "Failed to get times of job; invariant failed"
+              in job ++ case times of
+                          Just times' -> ";" ++ concatMap (pointPri times')
+                                         (timeRange start end stepSecs) ++ "\n"
+                          Nothing     -> error "Failed to get times of job; invariant failed"
 
 csvExport :: UTCTime -> UTCTime -> Map.Map String [UTCTime] -> String
 csvExport start end jobnamesMap = csvColumnTimestamps start end stepSecs ++
@@ -136,40 +136,40 @@ csvExport start end jobnamesMap = csvColumnTimestamps start end stepSecs ++
                                   csvData start end stepSecs jobnamesMap
     where stepSecs = 60
 
--- TODO just for debugging
-mkCronjobExecution jobname = do
-  now <- getCurrentTime
-  return (now, jobname)
+csvExport2 :: UTCTime -> UTCTime -> Map.Map String [UTCTime] -> String
+csvExport2 start end jobnamesMap =
+    "Jobname/Timestamp;" ++ csvColumnTimestamps start end stepSecs ++ "\n" ++
+    "Minutes;" ++ csvColumnMinutes start end stepSecs ++ "\n" ++
+    csvData start end stepSecs jobnamesMap
+        where stepSecs = 60
 
 s = parseIso8601Timestamp "2014-03-17T06:25"
-e = parseIso8601Timestamp "2014-03-17T06:30"
+e = parseIso8601Timestamp "2014-03-17T07:26"
 
 foo = do
   contents <- readFile "syslog-20140318"
   let logLines = lines contents
-  putStrLn $ "Got " ++ show(length logLines) ++ " lines"
   year <- getCurrentYear
-  putStrLn $ "Hello the current year is " ++ show year
   let dateParser = parseCronTimestamp year
   let database = parseCronSyslog dateParser logLines
-  putStrLn $ "Got " ++ show(length database) ++ " cron entries"
   let jobsBetween = between s e database
   putStrLn $ "Got " ++ show(length jobsBetween) ++ " jobs between start,end"
   let jobnamesMap = mapByJobnames database
-  return jobnamesMap
+  -- return jobnamesMap
+  putStrLn $ csvExport2 s e jobnamesMap
 
-main = do
-  contents <- readFile "syslog-20140318"
-  let logLines = lines contents
-  putStrLn $ "Got " ++ show(length logLines) ++ " lines"
-  year <- getCurrentYear
-  putStrLn $ "Hello the current year is " ++ show year
-  let dateParser = parseCronTimestamp year
-  let database = parseCronSyslog dateParser logLines
-  putStrLn $ "Got " ++ show(length database) ++ " cron entries"
-  let jobsBetween = between s e database
-  putStrLn $ "Got " ++ show(length jobsBetween) ++ " jobs between start,end"
-  let jobnamesMap = mapByJobnames database
-  putStrLn $ csvExport s e jobnamesMap
+-- main = do
+--   contents <- readFile "syslog-20140318"
+--   let logLines = lines contents
+--   putStrLn $ "Got " ++ show(length logLines) ++ " lines"
+--   year <- getCurrentYear
+--   putStrLn $ "Hello the current year is " ++ show year
+--   let dateParser = parseCronTimestamp year
+--   let database = parseCronSyslog dateParser logLines
+--   putStrLn $ "Got " ++ show(length database) ++ " cron entries"
+--   let jobsBetween = between s e database
+--   putStrLn $ "Got " ++ show(length jobsBetween) ++ " jobs between start,end"
+--   let jobnamesMap = mapByJobnames database
+--   putStrLn $ csvExport s e jobnamesMap
 
 -- EOF
